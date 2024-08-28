afa6a2eb413b8ccc54eeb80bb148f2a6ee69b196
patricio <patricio_rios2010@live.com>
2024-08-28 13:40:34 -0300

    documentacion

--- ui.c
+++ ui.c
@@ -102,7 +102,39 @@ void ordenarOptionsAscendente(Options *opts)
     }
   }
 }
+/*
+Example:
+    Option arr[8] = {{"Dar de alta un producto nuevo.", 1},
+                     {"Buscar un producto por su Código.", 1},
+                     {"Modificar el stock por venta.", 1},
+                     {"Modificar el Precio de un producto.", 1},
+                     {"Cuantos productos tiene faltante.", 1},
+                     {"A cuantos le faltan vender 3 producto para que quede el stock mínimo.", 1},
+                     {"Mostrar productos.", 1},
+                     {"Salir.", 1}};
+    Options options = {arr, 8};
+    menuOptions(options);
+    output:
+    ╔════════════════════════════════════════
+    ║ 1 - Dar de alta un producto nuevo.
+    ╠════════════════════════════════════════
+    ║ 2 - Buscar un producto por su Código.
+    ╠════════════════════════════════════════
+    ║ 3 - Modificar el stock por venta.
+    ╠════════════════════════════════════════
+    ║ 4 - Modificar el Precio de un producto.
+    ╠════════════════════════════════════════
+    ║ 5 - Cuantos productos tiene faltante.
+    ╠════════════════════════════════════════
+    ║ 6 - A cuantos le faltan vender 3 producto para que quede el stock mínimo.
+    ╠════════════════════════════════════════
+    ║ 7 - Mostrar productos.
+    ╠════════════════════════════════════════
+    ║ 8 - Salir.
+    ╠════════════════════════════════════════
 
+  Note: Order according to the order given in the arrangement, no enumeration needed
+*/
 void menuOptionsEnumerated(Options options)
 {
   topLine();
@@ -112,7 +144,39 @@ void menuOptionsEnumerated(Options options)
     midLine();
   }
 }
+/*
+Example:
+    Option arr[8] = {{"Dar de alta un producto nuevo.", 1},
+                     {"Buscar un producto por su Código.", 2},
+                     {"Modificar el stock por venta.", 3},
+                     {"Modificar el Precio de un producto.", 4},
+                     {"Cuantos productos tiene faltante.", 5},
+                     {"A cuantos le faltan vender 3 producto para que quede el stock mínimo.", 6},
+                     {"Mostrar productos.", 7},
+                     {"Salir.", 8}};
+    Options options = {arr, 8};
+    menuOptions(options);
+    output:
+    ╔════════════════════════════════════════
+    ║ 1 - Dar de alta un producto nuevo.
+    ╠════════════════════════════════════════
+    ║ 2 - Buscar un producto por su Código.
+    ╠════════════════════════════════════════
+    ║ 3 - Modificar el stock por venta.
+    ╠════════════════════════════════════════
+    ║ 4 - Modificar el Precio de un producto.
+    ╠════════════════════════════════════════
+    ║ 5 - Cuantos productos tiene faltante.
+    ╠════════════════════════════════════════
+    ║ 6 - A cuantos le faltan vender 3 producto para que quede el stock mínimo.
+    ╠════════════════════════════════════════
+    ║ 7 - Mostrar productos.
+    ╠════════════════════════════════════════
+    ║ 8 - Salir.
+    ╠════════════════════════════════════════
 
+  Note: No need order
+*/
 void menuOptions(Options options)
 {
   topLine();
